b7cd254ad8841ba584048c39c7a759a2aacc256e
Christina <111030842+Christinaty@users.noreply.github.com>
2022-12-16 00:34:41 +0100

    monty

--- pop.c
+++ pop.c
@@ -1,45 +1,32 @@
 #include "monty.h"
-
 /**
-
- * f_pop - prints the top
-
+ * f_mul - multiplies the top two elements of the stack.
  * @head: stack head
-
  * @counter: line_number
-
  * Return: no return
-
 */
-
-void f_pop(stack_t **head, unsigned int counter)
-
+void f_mul(stack_t **head, unsigned int counter)
 {
-
 	stack_t *h;
-
-
-
-	if (*head == NULL)
-
-	{
-
-		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
-
-		fclose(bus.file);
-
-		free(bus.content);
-
-		free_stack(*head);
-
-		exit(EXIT_FAILURE);
-
-	}
+	int len = 0, aux;
 
 	h = *head;
-
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->next->n * h->n;
+	h->next->n = aux;
 	*head = h->next;
-
 	free(h);
-
 }
